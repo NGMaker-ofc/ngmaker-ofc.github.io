68a6e0f26f10f2614a2c2bc32f82dfcf425fd36e
Not Zero <32226366+YSpoof@users.noreply.github.com>
2024-05-06 13:53:48 -0300

    Bun

---
+++ components/AboutUs.vue
@@ -0,0 +1,6 @@
+<template>
+<section id="aboutUs" class="bg-stone-900 px-8 pt-8 shadow-xl transition-all hover:scale-105">
+  <img class="hidden md:block" src="/images/about.webp" alt="AboutUs">
+  <img class="md:hidden" src="/images/aboutMobile.webp" alt="AboutUs">
+</section>
+</template>
