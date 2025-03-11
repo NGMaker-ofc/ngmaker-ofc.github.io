69aac2370e3c2ae89b0acc5298dd760d7740e922
YSpoof <32226366+YSpoof@users.noreply.github.com>
2025-03-11 15:53:36 -0300

    Windows Stuff

---
+++ Start_Windows.bat
@@ -0,0 +1,18 @@
+@echo off
+title Salvando alterações locais
+echo Salvando alterações locais...
+git stash
+echo Alterações locais salvas.
+title Sincronizando repositorio
+echo Sincronizando repositorio...
+git fetch -a
+git reset --hard origin/main
+git pull
+echo Repositorio sincronizado.
+title Restaurando alterações locais
+echo Restaurando alterações locais...
+git stash pop
+echo Alterações locais restauradas.
+title Iniciando servidor
+echo Iniciando servidor...
+bun run dev
